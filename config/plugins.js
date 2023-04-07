b0bb8de32685a3ad432026fbf3fcfaddfa38a951
Alexandre Henrique da Rocha Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2023-04-07 18:59:02 -0300

    update

---
+++ config/plugins.js
@@ -0,0 +1,13 @@
+
+module.exports = ({ env }) => ({
+
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET'),
+      jwt: {
+        expiresIn: '4h',
+      },
+    },
+  },
+
+});
