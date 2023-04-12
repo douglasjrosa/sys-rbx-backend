848fc7ec3332c375f7ca9399c35208383172d5fb
Alexandre Henrique da Rocha Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2023-04-11 23:24:56 -0300

    update

--- src/api/fornecedor/services/fornecedor.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * fornecedor service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::fornecedor.fornecedor');
---
+++ src/api/logstrello/routes/logstrello.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * logstrello router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::logstrello.logstrello');
